cf76c3b7172c6ef7088a0d66db33dbe36bd942c1
Kamil 1st <kamilferst@wp.pl>
2018-02-08 19:47:52 +0100

    poprawiony kod jquery

--- zadania-domowe/dane-programisty/js/main.js
+++ zadania-domowe/dane-programisty/js/main.js
@@ -8,6 +8,8 @@ $(document).ready(function () {
 
         $.getJSON("https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php", function (data) {
 
+            $("body").html("<div></div>");
+            $("div").attr("id","dane-programisty");
             $("#dane-programisty").html("<p></p>");
             $("p").text("Imię: " + data.imie + ", Nazwisko: " + data.nazwisko + ", Zawód: " + data.zawod + ", Firma: " + data.firma + ".")
         });
